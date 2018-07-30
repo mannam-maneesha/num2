1eeb4021a2a0fa6ccfdfa4a0707294bd721fb07b
mannam-maneesha <41893993+mannam-maneesha@users.noreply.github.com>
2018-07-30 16:37:07 +0530

    Create num2.java

---
+++ num2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	 { 
+	 	int number; 
+	 Scanner sc=new Scanner(System.in);
+	 number=sc.nextInt(); 
+	 if(number>0) 
+	 { 
+	 	System.out.println("Positive");
+     } 
+	 	else(number<0) 
+	 	{ 
+	 		System.out.println("Negative");
+	 	} 
+	 
+	}
+}
